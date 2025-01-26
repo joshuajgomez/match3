5eca1aaeb4d8136b7d3cfd6ebc91e8700f9e0914
joshua <joshuajgomez@gmail.com>
2025-01-26 13:09:46 +0100

    Re org

---
+++ app/src/main/java/com/joshgm3z/match3/model/EngineUtils.kt
@@ -0,0 +1,58 @@
+package com.joshgm3z.match3.model
+
+import com.joshgm3z.match3.ui.Item
+
+fun <Int> ArrayList<Int>.addUnique(i: Int) {
+    when {
+        !contains(i) -> add(i)
+    }
+}
+
+fun <E> ArrayList<E>.swap(
+    source: Int,
+    target: Int
+) {
+    val sourceTemp = this[source]
+    val targetTemp = this[target]
+    this[source] = targetTemp
+    this[target] = sourceTemp
+}
+
+inline fun ArrayList<Item?>.forEachRow(row: (ArrayList<Item>) -> Unit) {
+    repeat(10) {
+        val start = it * 10
+        val endRange = (it * 10) + 10
+        row(subList(start, endRange) as ArrayList<Item>)
+    }
+}
+
+inline fun ArrayList<Item?>.forEachCol(column: (ArrayList<Item>) -> Unit) {
+    repeat(10) { i ->
+        val col = ArrayList<Item>()
+        repeat(10) {
+            this.elementAt(it * 10 + i)?.let { it1 -> col.add(it1) }
+        }
+        column(col)
+    }
+}
+
+fun check3MatchPerList(list: List<Item>): List<Int> {
+    val matches = ArrayList<Int>()
+    var letter = ""
+    run loop@{
+        list.forEach {
+            when {
+                matches.size == 5 -> return@loop
+                letter == it.letter -> matches.add(it.position)
+                matches.size >= 3 -> return@loop
+                else -> matches.clear()
+            }
+            letter = it.letter
+            matches.addUnique(it.position)
+        }
+    }
+    return when {
+        matches.size >= 3 -> matches
+        else -> emptyList()
+    }
+}
--- app/src/main/java/com/joshgm3z/match3/model/GameEngine.kt
+++ app/src/main/java/com/joshgm3z/match3/model/GameEngine.kt
@@ -26,12 +26,6 @@ class GameEngine
         reposition()
     }
 
-    /**
-     * Checks if match of 3 or more items exist
-     * First searches all rows, then columns
-     */
-    fun checkMatch(): List<Int> = check3Match()
-
     fun removeItems(list: List<Int>) {
         println("Removing $list")
         list.forEach {
@@ -39,6 +33,10 @@ class GameEngine
         }
     }
 
+    /**
+     * Checks if match of 3 or more items exist
+     * First searches all rows, then columns
+     */
     private fun check3Match(): List<Int> {
         var match = listOf<Int>()
         run loop@{
@@ -63,63 +61,9 @@ class GameEngine
         return match
     }
 
-    private inline fun ArrayList<Item?>.forEachRow(row: (ArrayList<Item>) -> Unit) {
-        repeat(10) {
-            val start = it * 10
-            val endRange = (it * 10) + 10
-            row(subList(start, endRange) as ArrayList<Item>)
-        }
-    }
-
-    private inline fun ArrayList<Item?>.forEachCol(column: (ArrayList<Item>) -> Unit) {
-        repeat(10) { i ->
-            val col = ArrayList<Item>()
-            repeat(10) {
-                this.elementAt(it * 10 + i)?.let { it1 -> col.add(it1) }
-            }
-            column(col)
-        }
-    }
-
-    private fun check3MatchPerList(list: List<Item>): List<Int> {
-        val matches = ArrayList<Int>()
-        var letter = ""
-        run loop@{
-            list.forEach {
-                when {
-                    matches.size == 5 -> return@loop
-                    letter == it.letter -> matches.add(it.position)
-                    matches.size >= 3 -> return@loop
-                    else -> matches.clear()
-                }
-                letter = it.letter
-                matches.addUnique(it.position)
-            }
-        }
-        return when {
-            matches.size >= 3 -> matches
-            else -> emptyList()
-        }
-    }
-
     fun listItems(): List<Item?> {
         return items
     }
 
-    private fun <Int> ArrayList<Int>.addUnique(i: Int) {
-        when {
-            !contains(i) -> add(i)
-        }
-    }
-
-    private fun <E> ArrayList<E>.swap(
-        source: Int,
-        target: Int
-    ) {
-        val sourceTemp = this[source]
-        val targetTemp = this[target]
-        this[source] = targetTemp
-        this[target] = sourceTemp
-    }
 }
 
